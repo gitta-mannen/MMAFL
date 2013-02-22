2d17277db134e022a16d34aaa77866ae2d2c6041
gitta-mannen <mail@janpettersson.com>
2013-02-22 17:07:32 +0100

    Added databse handling code
    Signed-off-by: gitta-mannen <mail@janpettersson.com>

---
+++ code/server/src/database/Example.java
@@ -0,0 +1,12 @@
+package database;
+
+public class Example {
+	public static void main(String[] args) {
+		// used once for all scrapers
+		StatsHandler db = new StatsHandler();
+		// once per record (i.e once per event)
+		db.update(new Event(1, "UFC the Tuna Sandwich", "2012-02-01", "Hålanda", "UFC", "50000000"));
+		// once for all scrapers
+		db.close();
+	}
+}
