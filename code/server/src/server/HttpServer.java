e069c6221632e51a63c747aa85a31da3d148a24a
gitta-mannen <mail@janpettersson.com>
2013-02-24 03:10:18 +0100

    Did some work on webserver
    Signed-off-by: gitta-mannen <mail@janpettersson.com>

--- code/server/src/server/HttpServer.java
+++ code/server/src/server/HttpServer.java
@@ -56,8 +56,8 @@ class HttpRequest implements Runnable{
 	private StatsHandler db;
     private Socket socket;
     final static String CRLF = "\r\n";
-    final static File webRoot = new File("\\www\\");
-    final static long timeOut = 500;
+    // Shouldn't be used in production
+    final static File webRoot = new File(System.getProperty("user.dir") + "\\www\\");
     private InputStreamReader is;
 	private DataOutputStream os;
     
@@ -69,12 +69,11 @@ class HttpRequest implements Runnable{
     @Override
     final public void run() {
     	 System.out.println("*** Server accepted new connection on port: " + socket.getPort() +
-                 " thread id: " + Thread.currentThread().getId() +  " ," + Thread.activeCount() + " active threads" + " ***");
+                 " thread id: " + Thread.currentThread().getId() +  " ***");
     
-    	long startTime;
+    	long startTime = System.currentTimeMillis();
     	String requestLine;
-    	HashMap request;
-    	boolean entered;
+    	HashMap<String, String> request;
     	db = new StatsHandler();
     	
 	    try {
@@ -86,45 +85,35 @@ class HttpRequest implements Runnable{
 			// Set up input stream filters.
 			BufferedReader br = new BufferedReader (is);
 	        // Extract the request fields
-			int i = 0;
-			int o = 0;
 			StringTokenizer fields;
-			request = new HashMap();
-			startTime = System.currentTimeMillis();
+			request = new HashMap<String, String>();	
+			boolean firstLine = true;
 			
-			while (true) {								
-				System.out.println("	>> Outer: " + ++o);				
-				entered = false;
-		        while ((br.ready() && (requestLine = br.readLine()).length() != 0) ) {		        	
-		        	System.out.println("	>> Inner: " + ++i);
-		        	System.out.println(requestLine);
-		        	fields = new StringTokenizer(requestLine, ": ");
-		        	request.put(fields.nextToken(), fields.nextToken(""));		
-		        	entered = true;
-		        }
-		        
-		        if (startTime + timeOut < System.currentTimeMillis()) {
-		        	System.out.println("	>>> timed out, leaving");	
-		        	break;
-		        } else if (entered) {
-		        	// Data was read, call handle method and then reset timer
-		        	startTime = System.currentTimeMillis();
-		        	System.out.println("	>>>data read");
-		        	processRequest(request);
-		        	request.clear();
-		        }
-		          else {		        	  
-		        	  System.out.println("	>>>Waiting for: " + (System.currentTimeMillis() - startTime));
-		        	  Thread.sleep(100);
-		        }
-            		        
-			}
-			
+	        while ((br.ready() && (requestLine = br.readLine()).length() != 0) ) {
+	        	System.out.println(requestLine);
+	        	fields = new StringTokenizer(requestLine, ": ");	     
+	        	
+	        	if(firstLine && fields.countTokens() == 3) {
+	        		request.put("Request-Method", fields.nextToken());
+	        		request.put("Request-URL", fields.nextToken());
+	        		request.put("Http-Version", fields.nextToken());
+	        		firstLine = false;
+	        	} else {
+	        		request.put(fields.nextToken(), fields.nextToken("").substring(2));
+	        	}
+	        }	        	       
+	        
+	        if(!request.isEmpty()) {
+		        System.out.println(request.toString());
+	        	processRequest(request);
+	        }
+
 			// Close DB connection, sockets and streams
 			db.close();             
 			br.close();
+			os.close();
 			socket.close();
-			System.out.println("Closing thread: " + Thread.currentThread().getId());	    
+			System.out.println("Thread: " + Thread.currentThread().getId() + " Closing thread");	    
 			 
         } catch (Exception e) {
             System.out.println(e);
@@ -142,38 +131,92 @@ class HttpRequest implements Runnable{
         // Construct the response message.
         String statusLine = null;
         String contentTypeLine = null;
-        StringBuilder entityBody = new StringBuilder();
+        String requestUrl = null;
        
-        statusLine = "HTTP/1.0 200 OK";
-        contentTypeLine = "Content-type: " + 
-                contentType( request.get("Request URL").toString() ) + CRLF;     
-        
-        // build html body
-        ArrayList<Event> events = db.get(new Event());
-        for (Event event : events) {
-        	entityBody.append(event.toHtmlString());
-		}
-				                         
-        // Send the status line.
-        os.writeBytes(statusLine);
-
-        // Send the content type line.
-        os.writeBytes(contentTypeLine);
-
-        // Send a blank line to indicate the end of the header lines.
-        os.writeBytes(CRLF);        
-
-        os.writeBytes("<HTML> <HEAD> <TITLE> Events </TITLE> </HEAD> <BODY> <table border=''1''> ");
-        os.writeBytes("<tr> <th> id </th> <th> name </th> <th> date </th>  <th> location </th> <th> organization </th> <th> attendence </th> </tr>");
-        os.writeBytes(entityBody.toString());
-        os.writeBytes("</table> </BODY> </HTML>");
-        
-        // Close socket.
-        os.close();       
+        if (request.containsKey("Request-URL") && request.containsKey("Request-Method")) {          
+        	if (request.get("Request-Method").toString().equals("GET")) {
+        		requestUrl = request.get("Request-URL").toString();
+                                
+                if (requestUrl.equals("/")) {
+                	requestUrl = "/index.html";                	
+                }
+                
+                File file = new File(webRoot, requestUrl);
+                System.out.println("Client sent request for: " + file.getPath());
+                
+                if (requestUrl.endsWith("events.html") || requestUrl.endsWith("events.html/")) {
+                	System.out.println("sending events");
+                	StringBuilder page = new StringBuilder();
+                	
+            		page.append("HTTP/1.0 200 OK");
+            		page.append(CRLF);
+                    page.append("Content-type: text/html");
+                    page.append(CRLF);
+                    page.append(CRLF);
+                    // build html body
+                    page.append("<!DOCTYPE HTML PUBLIC &quot-//W3C//DTD HTML 4.01 Transitional//EN&quot &quothttp://www.w3.org/TR/html4/loose.dtd&quot>");
+                    page.append(CRLF);
+                    page.append("<HTML><HEAD><TITLE>Events</TITLE></HEAD> <BODY><table border='1'>");
+                    page.append(CRLF);
+                    page.append("<tr><th> id </th><th> name </th><th> date </th><th> location </th><th> organization </th><th> attendence </th></tr>");
+                    page.append(CRLF);
+                    
+                    ArrayList<Event> events = db.get(new Event());
+                    for (Event event : events) {
+                    	page.append(event.toHtmlString());
+                    	page.append(CRLF);
+            		}                    
+ 
+                    page.append("</table></BODY></HTML>");
+                    page.append(CRLF);
+                    os.writeBytes(page.toString());
+                    
+                } else {
+                	FileInputStream fis = null;
+                    boolean fileExists = true;
+                    try {
+                    	fis = new FileInputStream(file.getPath());
+                    } catch (FileNotFoundException e) {
+                        fileExists = false;
+                    }
+                    
+                    if (fileExists)	{
+                    	System.out.println(requestUrl);
+                        sendBytes(fis, os);
+                        fis.close();
+                        os.writeBytes(CRLF);
+                        
+	                } else {
+	                	System.out.println("sending not found message");
+	                	
+	                	statusLine = "HTTP/1.0 404 Not Found";
+	                    contentTypeLine = "Content-type:  text/html" + CRLF;
+	                    String fileMissing = "<HTML>" + 
+	                            "<HEAD><TITLE>Not Found</TITLE></HEAD>" +
+	                            "<BODY>The requested file was not found on the server.</BODY></HTML>";	          
+	                    // Send the status line.
+	                    os.writeBytes(statusLine);
+	                    // Send the content type line.
+	                    os.writeBytes(contentTypeLine);
+	                    // Send a blank line to indicate the end of the header lines.
+	                    os.writeBytes(CRLF);      
+	                    os.writeBytes(fileMissing);
+	                    os.writeBytes(CRLF);
+	                    
+	                }
+                }
+        		
+        	} else {
+        		System.out.println("Request method not recognized");
+        	}
+            
+        } else {
+        	System.out.println("Headers missing");
+        }
            
     }
     
-    /* not used
+  
     private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception {
        // Construct a 1K buffer to hold bytes on their way to the socket.
        byte[] buffer = new byte[1024];
@@ -184,10 +227,10 @@ class HttpRequest implements Runnable{
           os.write(buffer, 0, bytes);
        }
     }
-    */
+
     private static String contentType(String fileName)
     {
-            if(fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+            if(fileName == "/" || fileName.endsWith(".htm") || fileName.endsWith(".html")) {
                     return "text/html";
             }
             if( fileName.endsWith(".gif") ) {
