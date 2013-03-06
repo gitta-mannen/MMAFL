563f1f64f8d9b2d825891c2d10651bd1b9392760
gitta-mannen <mail@janpettersson.com>
2013-03-06 17:01:53 +0100

    Added some comments to Scraper
    Signed-off-by: gitta-mannen <mail@janpettersson.com>

--- code/server/src/scraper/Scraper.java
+++ code/server/src/scraper/Scraper.java
@@ -16,24 +16,32 @@ import settings.Column;
 import settings.Settings;
 import util.Logger;
 
+// Private constructor, can't be instantiated.
 public class Scraper {
 	private Scraper() {};		
+	// Scrapes the given URL using regex taken from the settings object (which gets it from the xml settings file)
 	public static HashMap<String, Object> scrape(URL url, String table) throws MalformedURLException {
 		HashMap<String, Object> values = new HashMap<String, Object>();
 		try {
-			String htmlContent = util.Io.streamToString(url.openConnection().getInputStream());			
+			// Opens a stream to the given URL and copies the html content in to a String
+			String htmlContent = util.Io.streamToString(url.openConnection().getInputStream());		
+			// Iterates over the columns in the given table
 			Iterator<Entry<String, Column>> itrColumns = Settings.getInstance().getSchema().getTable(table).getColumns().entrySet().iterator();						
 			while (itrColumns.hasNext()) {
 				Entry<String, Column> column = itrColumns.next();
+				// Checks if a regex entry exists in the xml file for the given column and executes it on the html content.
 				if (column.getValue().regex != null && column.getValue().regex.length() > 0) {	
 					Pattern pattern = Pattern.compile(column.getValue().regex, Pattern.MULTILINE);
-					Matcher matcher = pattern.matcher(htmlContent);						
+					Matcher matcher = pattern.matcher(htmlContent);
+					// The regex matches 3 groups, before the data, the data (e.g. stance), and after the data.
+					// The second group (the data) is put in the string 'match'.
 					while(matcher.find() && matcher.groupCount() == 3) {	
-						String match = matcher.group(2);						
+						String match = matcher.group(2);				
+							// The data is formated and converted to the proper type then put in the hashmap with the column name as key.
 							if (column.getValue().apptype.equals("date")) {
-								values.put(column.getKey(), new SimpleDateFormat("MMM. dd, yyyy").parse(match) );
+								values.put(column.getKey(), new SimpleDateFormat("MMM. dd, yyyy").parse(match) ); // parses the date format used on Fightmetric
 							} else if (column.getValue().apptype.equals("long") || column.getValue().apptype.equals("double") || column.getValue().apptype.equals("integer")) {
-								values.put(column.getKey(), NumberFormat.getInstance(Locale.US).parseObject(match) );
+								values.put(column.getKey(), NumberFormat.getInstance(Locale.US).parseObject(match) ); // Parses numbers written in a US locale
 							} else if (column.getValue().apptype.equals("string")){
 								values.put(column.getKey(), match);
 							} else {
@@ -48,6 +56,7 @@ public class Scraper {
 		return values;
 	}		
 	
+	// Calls the scrape method for a range of <from-to>. Takes the table name and base url as arguments
 	public static void scrapeRangeToDb(String table, String baseUrl, int from, int to) throws MalformedURLException {
 		DbHandler db = new DbHandler();									
 		for (int id = from; id <= to; id++) {
@@ -57,7 +66,8 @@ public class Scraper {
 		}
 		db.close();
 	}
-
+	
+	// test method for scraping iteratively
 	public static void main(String[] args) throws MalformedURLException {
 		scrapeRangeToDb("fighters", "http://hosteddb.fightmetric.com/fighters/details/", 3, 4);
 		scrapeRangeToDb("events", "http://hosteddb.fightmetric.com/events/details/", 1, 2);		
